b8cb15f77c51fc67eaf79784058464c9f70e09b6
يُمن <yomnamohammed262004@gmail.com>
2026-02-20 22:36:33 +0200

    nav_links

---
+++ apps/web/constants/nav_links.ts
@@ -0,0 +1,11 @@
+export const nav_links = [
+  { href: '/', label: 'Stream', key: 'stream' },
+  { href: '/classwork', label: 'Classwork', key: 'classwork' },
+  { href: '/people', label: 'People', key: 'people' },
+];
+
+export interface NavLink {
+  href: string;
+  key: string;
+  label: string;
+}
