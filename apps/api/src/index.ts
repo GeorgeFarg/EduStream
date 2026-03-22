14d34e4a0bd8884c444b0b56f9f3d859a8363b01
youssefayman1486 <youssefayman1486@gmail.com>
2026-03-22 18:22:03 +0200

    feat(api): integrate Socket.IO for real-time chat functionality; update dependencies

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,13 +3,36 @@ import type { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import app from "./app";
+import { redisClient } from "./config/redis";
+import { prisma } from "../lib/prisma";
+import { createServer } from "node:http";
+import { Server } from 'socket.io';
+
+
+const server = createServer(app);
+const io = new Server(server);
+
+
+import { chatHandler } from "./sockets/chat.socket";
+
+io.on("connection", (socket) => {
+  chatHandler(io, socket);
+});
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+
+
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
